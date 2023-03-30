8a246c90c35708775613bdf022ce4c6e9f4cb5a9
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-03-31 00:40:25 +0300

    IpRateLimitPolicies

---
+++ RateLimit.API/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimit.API.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetCategory()
+        {
+            return Ok(new { Id = 1, Category = "Kırtasiye" });
+        }
+
+        [HttpPost]
+        public IActionResult SaveProduct()
+        {
+            return Ok(new { Status = "Success" });
+        }
+    }
+}
